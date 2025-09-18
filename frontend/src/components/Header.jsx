705a90fe2d5f12471f8c39cbf539a45beabf2574
carlosbsuedu <carlos.lazo@bsu.edu>
2025-09-17 22:18:42 -0700

    changes to frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { useAuth } from "../contexts/AuthContext.jsx";
+import { User } from "./User.jsx";
+
 export function Header() {
   const [token, setToken] = useAuth();
   if (token) {
     const { sub } = jwtDecode(token);
     return (
       <div>
-                Logged in as <b>{sub}</b>
+                Logged in as <User id={sub} />
                 <br />       {" "}
         <button onClick={() => setToken(null)}>Logout</button>     {" "}
       </div>
