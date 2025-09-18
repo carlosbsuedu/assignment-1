a4dc5ac5ee60abc4343112edd05ae4a3bd771229
carlosbsuedu <carlos.lazo@bsu.edu>
2025-09-17 22:43:08 -0700

    fix tests

--- backend/src/__tests__/posts.test.js
+++ backend/src/__tests__/posts.test.js
@@ -1,17 +1,34 @@
-import { describe, expect, test, beforeEach } from "@jest/globals";
-import { getPostById, updatePost, deletePost } from "../services/posts.js";
+import { describe, expect, test, beforeEach, beforeAll } from "@jest/globals";
+import mongoose from "mongoose";
+import {
+  createPost,
+  listAllPosts,
+  listPostsByAuthor,
+  listPostsByTag,
+  getPostById,
+  updatePost,
+  deletePost,
+} from "../services/posts.js";
 import { Post } from "../db/models/post.js";
-const samplePosts = [
-  { title: "Learning Redux", author: "Daniel Bugl", tags: ["redux"] },
-  { title: "Learn React Hooks", author: "Daniel Bugl", tags: ["react"] },
-  {
-    title: "Full-Stack React Projects",
-    author: "Daniel Bugl",
-    tags: ["react", "nodejs"],
-  },
-  { title: "Guide to TypeScript" },
-];
+import { createUser } from "../services/users.js";
+
+let testUser = null;
+//Remove old Sample posts with this one
+let samplePosts = [];
 let createdSamplePosts = [];
+beforeAll(async () => {
+  testUser = await createUser({ username: "sample", password: "user" });
+  samplePosts = [
+    { title: "Learning Redux", author: testUser._id, tags: ["redux"] },
+    { title: "Learn React Hooks", author: testUser._id, tags: ["react"] },
+    {
+      title: "Full-Stack React Projects",
+      author: testUser._id,
+      tags: ["react", "nodejs"],
+    },
+  ];
+});
+
 beforeEach(async () => {
   await Post.deleteMany({});
   createdSamplePosts = [];
@@ -20,6 +37,7 @@ beforeEach(async () => {
     createdSamplePosts.push(await createdPost.save());
   }
 });
+
 describe("getting a post", () => {
   test("should return the full post", async () => {
     const post = await getPostById(createdSamplePosts[0]._id);
@@ -32,22 +50,22 @@ describe("getting a post", () => {
 });
 describe("updating posts", () => {
   test("should update the specified property", async () => {
-    await updatePost(createdSamplePosts[0]._id, {
-      author: "Test Author",
+    await updatePost(testUser._id, createdSamplePosts[0]._id, {
+      contents: "Some content change",
     });
     const updatedPost = await Post.findById(createdSamplePosts[0]._id);
-    expect(updatedPost.author).toEqual("Test Author");
+    expect(updatedPost.contents).toEqual("Some content change");
   });
   test("should not update other properties", async () => {
-    await updatePost(createdSamplePosts[0]._id, {
-      author: "Test Author",
+    await updatePost(testUser._id, createdSamplePosts[0]._id, {
+      contents: "Some content change",
     });
     const updatedPost = await Post.findById(createdSamplePosts[0]._id);
     expect(updatedPost.title).toEqual("Learning Redux");
   });
   test("should update the updatedAt timestamp", async () => {
-    await updatePost(createdSamplePosts[0]._id, {
-      author: "Test Author",
+    await updatePost(testUser._id, createdSamplePosts[0]._id, {
+      contents: "Some content change",
     });
     const updatedPost = await Post.findById(createdSamplePosts[0]._id);
     expect(updatedPost.updatedAt.getTime()).toBeGreaterThan(
@@ -55,15 +73,15 @@ describe("updating posts", () => {
     );
   });
   test("should fail if the id does not exist", async () => {
-    const post = await updatePost("000000000000000000000000", {
-      author: "Test Author",
+    const post = await updatePost(testUser._id, "000000000000000000000000", {
+      contents: "Some content change",
     });
     expect(post).toEqual(null);
   });
 });
 describe("deleting posts", () => {
   test("should remove the post from the database", async () => {
-    const result = await deletePost(createdSamplePosts[0]._id);
+    const result = await deletePost(testUser._id, createdSamplePosts[0]._id);
     expect(result.deletedCount).toEqual(1);
     const deletedPost = await Post.findById(createdSamplePosts[0]._id);
     expect(deletedPost).toEqual(null);
@@ -71,5 +89,30 @@ describe("deleting posts", () => {
   test("should fail if the id does not exist", async () => {
     const result = await deletePost("000000000000000000000000");
     expect(result.deletedCount).toEqual(0);
+  });
+});
+describe("creating posts", () => {
+  test("with all parameters should succeed", async () => {
+    const post = {
+      title: "Hello Mongoose!",
+      contents: "This post is stored in a MongoDB database using Mongoose.",
+      tags: ["mongoose", "mongodb"],
+    };
+
+    // create the post with a testUser._id as the author
+    const createdPost = await createPost(testUser._id, post);
+
+    // createdPost should have an ObjectId
+    expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId);
+
+    // retrieve it directly from the database
+    const foundPost = await Post.findById(createdPost._id);
+
+    // foundPost should contain all the fields of 'post'
+    expect(foundPost).toEqual(expect.objectContaining(post));
+
+    // timestamps should be Date instances
+    expect(foundPost.createdAt).toBeInstanceOf(Date);
+    expect(foundPost.updatedAt).toBeInstanceOf(Date);
   });
 });
--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,27 +1,26 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useAuth } from "../contexts/AuthContext.jsx";
 import { createPost } from "../api/posts.js";
-
 export function CreatePost() {
   const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
   const [contents, setContents] = useState("");
+  const [token] = useAuth();
   const queryClient = useQueryClient();
-
   const createPostMutation = useMutation({
-    mutationFn: () => createPost({ title, author, contents }),
+    mutationFn: () => createPost(token, { title, contents }),
     onSuccess: () => queryClient.invalidateQueries(["posts"]),
   });
-
   const handleSubmit = (e) => {
     e.preventDefault();
     createPostMutation.mutate();
   };
-
+  if (!token) return <div>Please log in to create new posts.</div>;
   return (
     <form onSubmit={handleSubmit}>
+           {" "}
       <div>
-        <label htmlFor="create-title">Title: </label>{" "}
+                <label htmlFor="create-title">Title: </label>       {" "}
         <input
           type="text"
           name="create-title"
@@ -29,43 +28,27 @@ export function CreatePost() {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
+             {" "}
       </div>
-
-      <br />
-
-      <div>
-        <label htmlFor="create-author">Author: </label>{" "}
-        <input
-          type="text"
-          name="create-author"
-          id="create-author"
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-      </div>
-
-      <br />
-
+            <br />
       <textarea
         value={contents}
         onChange={(e) => setContents(e.target.value)}
       />
-
-      <br />
-      <br />
-
+            <br />
+            <br />     {" "}
       <input
         type="submit"
-        value={createPostMutation.isPending ? "Creating..." : "Create"}
-        disabled={!title || createPostMutation.isPending}
+        value={createPostMutation.isPending ? "Creating...." : "Create"}
+        disabled={!title}
       />
-
+           {" "}
       {createPostMutation.isSuccess ? (
         <>
-          <br />
-          Post created successfully!{" "}
+                    <br />          Post created successfully!        {" "}
         </>
       ) : null}
+         {" "}
     </form>
   );
 }
