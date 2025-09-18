ef11e35d6a56cee2f08f59e57dd9d2f625cf6d19
carlosbsuedu <carlos.lazo@bsu.edu>
2025-09-17 22:11:57 -0700

    wip

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Blog } from "./pages/Blog.jsx";
 import { Signup } from "./pages/Signup.jsx";
+import { Login } from "./pages/Login.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext.jsx";
 const queryClient = new QueryClient();
 const router = createBrowserRouter([
   {
@@ -12,12 +14,19 @@ const router = createBrowserRouter([
     path: "/signup",
     element: <Signup />,
   },
+  {
+    path: "/login",
+    element: <Login />,
+  },
 ]);
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+           {" "}
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
     </QueryClientProvider>
   );
 }
